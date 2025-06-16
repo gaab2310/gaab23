5faca2a0e06e57d9ee790f7bcf161c9ec7913787
joao vitor <215539112+gaab2310@users.noreply.github.com>
2025-06-16 11:30:44 -0400

    passagem

---
+++ funcao/passagem.js
@@ -0,0 +1,6 @@
+function adicionaritem(lista){
+    lista PushManager("Novo item");
+}
+let itens = ["item 1","item 2"];
+adicionaritem(itens);
+console.log(itens); // ["ITEM 1","item 2", "novo item"]
