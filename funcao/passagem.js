b14e9cf1fe973f7c2ae48dd206acd6395342a45d
joao vitor <215539112+gaab2310@users.noreply.github.com>
2025-06-26 10:38:06 -0400

    cadastro

--- funcao/passagem.js
+++
@@ -1,6 +0,0 @@
-function adicionaritem(lista){
-    lista PushManager("Novo item");
-}
-let itens = ["item 1","item 2"];
-adicionaritem(itens);
-console.log(itens); // ["ITEM 1","item 2", "novo item"]
